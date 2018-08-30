2777b2bf6fea3cf9f1904e00fc8e1cfe0e7c7aa0
lcagustini <torresmon235@gmail.com>
2018-08-30 10:38:49 -0300

    Fixed some monster losing track of their path

--- source/main.c
+++ source/main.c
@@ -75,7 +75,7 @@ int main(void){
     u16 dt = 0;
     u16 secondTimer = 0;
     u8 pressed = FALSE;
-    u8 needsDijkstra = FALSE;
+    u8 needsDijkstra[MONSTER_COUNT] = {0};
     while(1){
         scanKeys();
         pressedKeys = keysDown();
@@ -104,7 +104,9 @@ int main(void){
                     o.range = 1;
                     newTower(&w, o);
 
-                    needsDijkstra = TRUE;
+                    for (int i = 0; i < w.monsterNumber; i++) {
+                        needsDijkstra[i] = TRUE;
+                    }
                 }
             }
             pressed = TRUE;
@@ -160,6 +162,8 @@ int main(void){
             s8 dx = x - x0;
             s8 dy = y - y0;
 
+            PRINT("%d -> path=%d x= %d y=%d\n", i, cur->cur_path_index, x, y);
+
             if (dx) {
                 sassert(dx == -1 || dx == 1, "Invalid path");
                 cur->pos.x += mulf32(cur->speed, inttof32(dx));
@@ -197,14 +201,12 @@ int main(void){
                 if (dy == 1 && y == 12) {
                     switchMonsterScreen(&w, i);
                 }
-            }
-        }
 
-        if (needsDijkstra) {
-            for (int j = 0; j < w.monsterNumber; j++) {
-                dijkstra(&w, j);
+                if (needsDijkstra[i]) {
+                    dijkstra(&w, i);
+                    needsDijkstra[i] = FALSE;
+                }
             }
-            needsDijkstra = FALSE;
         }
 
         for (int i = 0; i < w.towerNumber; i++) {
